db11248ed56046e06290b24ed387750e38a69600
pie18033 <jason1803383@gmail.com>
2026-04-16 01:07:12 +0800

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -360,7 +360,7 @@ else:
                 updatemenus=[dict(
                     type="buttons",
                     direction="right",
-                    x=1.0,                  # 💡 關鍵修改 1：改為 1.0 (移到最右側)
+                    x=0.85,                  # 💡 關鍵修改 1：改為 1.0 (移到最右側)
                     y=1.12,                 # 保持在圖表上方的高度
                     xanchor="right",        # 💡 關鍵修改 2：向右對齊，確保按鈕不會超出螢幕邊緣
                     yanchor="bottom",
